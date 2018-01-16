4fb78fb4b8a8bcbab7606ac9555b04bca5207434
Ken Howard <ken@simplicatedweb.com>
2018-01-15 16:48:03 -0800

    feat(logger): Add logger. Refactor code validation.

--- server/controllers/access-codes.controller.ts
+++ server/controllers/access-codes.controller.ts
@@ -4,12 +4,42 @@ import * as Joi from 'joi';
 
 import { getDBContext, DbContext } from '../models';
 import { Mailer } from '../infrastructure/mailer';
+import { logger } from '../logger';
+import { AccessCodeAttributes } from '../models/access-code.model';
+import { access } from 'fs';
+import { ClientAttributes } from '../models/client.model';
 
 const notificationTemplates = {
     AirlineOrderConfirmation: '2tix-airline-order-confirmation',
     CruiseOrderConfirmation: '2tix-cruise-order-confirmation',
     InternalOrderConfirmation: '2tix-internal-confirmation',
     ErrorOccurred: '2tix-internal-error-occurred'
+}
+
+enum ErrorCode {
+    CODE_NOT_FOUND = 100,
+    CODE_NOT_ACTIVE,
+    CLIENT_NOT_FOUND,
+    CLIENT_NOT_ACTIVE,
+    CODE_LIMIT_EXCEEDED,
+    CODE_START_DATE_IN_FUTURE,
+    CODE_EXPIRE_DATE_IN_PAST
+}
+
+const accessCodeErrors = {
+    [ErrorCode.CODE_NOT_FOUND]: '',
+    [ErrorCode.CODE_NOT_ACTIVE]: '',
+    [ErrorCode.CLIENT_NOT_FOUND]: '',
+    [ErrorCode.CLIENT_NOT_ACTIVE]: '',
+    [ErrorCode.CODE_LIMIT_EXCEEDED]: 'The maximum redemptions for this code has been exceeded.',
+    [ErrorCode.CODE_START_DATE_IN_FUTURE]: 'The start date is in the future.',
+    [ErrorCode.CODE_EXPIRE_DATE_IN_PAST]: 'The expire date is in the past.'
+}
+
+const getErrorDetails = (errorCode: ErrorCode): { errorCode: string; errorMessage: string; } => {
+    const errorCodeStr = ErrorCode[errorCode];
+    const errorMessage = accessCodeErrors[errorCode];
+    return { errorCode: errorCodeStr, errorMessage };
 }
 
 @controller('/api/accesscodes')
@@ -21,6 +51,7 @@ class AccessCodesController implements Controller {
     baseUrl: string;
     routes: () => RouteConfiguration[];
 
+    // TODO: Add security
     @get('/')
     getAccessCodes(request: Request, reply: ReplyNoContinue): void {
         const { code } = <{ [key: string]: string }>request.query;
@@ -32,6 +63,8 @@ class AccessCodesController implements Controller {
 
         this._db.AccessCode.findAll({ where }).then(accessCodes => {
             reply({ data: accessCodes });
+        }).catch(error => {
+            logger.error(error);
         });
     }
 
@@ -41,40 +74,36 @@ class AccessCodesController implements Controller {
             code: Joi.string().required()
         })
     })
-    async validateAccessCode(request: Request, reply: ReplyNoContinue) {
+    async validate(request: Request, reply: ReplyNoContinue) {
         const { code } = request.query;
         try {
-            const accessCode = await this._db.AccessCode.findOne({ where: { Code: code } });
-            const client = (accessCode) ? await this._db.Client.findById(accessCode.get('ClientId')) : null;
-            const today = new Date();
-            let error: string;
-            let valid = false;
+            const { valid, accessCode, client, errorCode, errorMessage } = await this.validateAccessCode(code);
+            /* tslint:enable max-line-length */
 
-            if (!accessCode || accessCode.get('Active') === false) {
-                // Not an error... but invalid
-            } else if (!client || client.get('Active') === false) {
-                // Not an error... but invalid
-            } else if (accessCode.get('StartDate') && accessCode.get('StartDate') > today) {
-                error = `The access code start date is ${accessCode.get('StartDate')}. Today is ${today}. Use of this code has been denied.`;
-            } else if (accessCode.get('ExpireDate') && accessCode.get('ExpireDate') < today) {
-                error = `The access code expire date is ${accessCode.get('ExpireDate')}. Today is ${today}. Use of this code has been denied.`;
-            } else if (accessCode.get('MaxQuantity') && accessCode.get('UsedQuantity') && accessCode.get('MaxQuantity') <= accessCode.get('UsedQuantity')) {
-                error = 'The access code has been redeemed the maximun number of times. Use of this code has been denied.';
-            } else {
-                valid = true;
-            }
+            const errorsToNotifyClient = [
+                ErrorCode[ErrorCode.CODE_START_DATE_IN_FUTURE],
+                ErrorCode[ErrorCode.CODE_EXPIRE_DATE_IN_PAST],
+                ErrorCode[ErrorCode.CODE_LIMIT_EXCEEDED]
+            ];
 
-            if (error && client) {
+            if (
+                valid !== true &&
+                client !== null &&
+                client !== undefined &&
+                errorCode !== null &&
+                errorCode !== undefined &&
+                errorsToNotifyClient.indexOf(errorCode) > -1) {
+
                 await this._mailer.send({
                     recipients: [
                         {
                             address: {
-                                name: client.get('ContactName'),
-                                email: client.get('Email')
+                                name: client.ContactName,
+                                email: client.Email
                             },
                             substitution_data: {
-                                CodeName: accessCode.get('Code'),
-                                ErrorMessage: error
+                                CodeName: accessCode.Code,
+                                ErrorMessage: [errorMessage, 'Use of this code has been denied.'].join(' ')
                             }
                         }
                     ],
@@ -84,13 +113,25 @@ class AccessCodesController implements Controller {
                 });
             }
 
-            reply({ data: { valid } });
+            if (valid !== true) {
+                logger.info(
+                    ['Invalid Access Code', errorMessage].join(': '),
+                    {
+                        errorCode,
+                        accessCode: (accessCode && accessCode.Code) || code
+                    }
+                );
+            }
+
+            reply({ data: { valid, errorCode, errorMessage } });
 
         } catch (error) {
+            logger.error(error);
             reply(error);
         }
     }
 
+    // TODO: Add security
     @post('/')
     @validate({
         payload: getDBContext().validations.AccessCode.options({ stripUnknown: true })
@@ -104,7 +145,7 @@ class AccessCodesController implements Controller {
                 const errors: ApiErrors = [];
                 let detail = 'Something went wrong';
                 let statusCode = 500;
-                switch(error.message) {
+                switch (error.message) {
                     case 'DUPLICATE':
                         detail = 'The access code you entered is already in use.';
                         statusCode = 422;
@@ -128,6 +169,85 @@ class AccessCodesController implements Controller {
         this._mailer = mailer;
         return this;
     }
+
+    // tslint:disable-next-line:max-line-length
+    private async validateAccessCode(code: string): Promise<{ valid: boolean; accessCode?: AccessCodeAttributes; client?: ClientAttributes; errorCode?: string; errorMessage?: string; }> {
+        const accessCode = await this._db.AccessCode.findOne({ where: { Code: code } });
+        const today = new Date();
+        let accessCodeJson: AccessCodeAttributes;
+        let client;
+
+        const returnObj = {
+            valid: false
+        };
+
+        if (accessCode === null || accessCode === undefined) {
+            return Promise.resolve({
+                ...returnObj,
+                ...getErrorDetails(ErrorCode.CODE_NOT_FOUND)
+            });
+        }
+
+        accessCodeJson = accessCode.toJSON();
+
+        returnObj['accessCode'] = accessCodeJson;
+
+        const {
+            Active,
+            ClientId,
+            StartDate,
+            ExpireDate,
+            MaxQuantity,
+            UsedQuantity
+        } = accessCodeJson;
+
+        if (Active !== true) {
+            return Promise.resolve({
+                ...returnObj,
+                ...getErrorDetails(ErrorCode.CODE_NOT_ACTIVE)
+            });
+        }
+
+        client = await this._db.Client.findById(ClientId);
+
+        if (client === null || client === undefined) {
+            return Promise.resolve({
+                ...returnObj,
+                ...getErrorDetails(ErrorCode.CLIENT_NOT_FOUND)
+            });
+        }
+
+        returnObj['client'] = client.toJSON();
+
+        if (client.get('Active') !== true) {
+            return Promise.resolve({
+                ...returnObj,
+                ...getErrorDetails(ErrorCode.CLIENT_NOT_ACTIVE)
+            });
+        }
+
+        if (StartDate && StartDate > today) {
+            return Promise.resolve({
+                ...returnObj,
+                ...getErrorDetails(ErrorCode.CODE_START_DATE_IN_FUTURE)
+            });
+        } else if (accessCode.get('ExpireDate') && accessCode.get('ExpireDate') < today) {
+            return Promise.resolve({
+                ...returnObj,
+                ...getErrorDetails(ErrorCode.CODE_EXPIRE_DATE_IN_PAST)
+            });
+        } else if (MaxQuantity && UsedQuantity && MaxQuantity <= UsedQuantity) {
+            return Promise.resolve({
+                ...returnObj,
+                ...getErrorDetails(ErrorCode.CODE_LIMIT_EXCEEDED)
+            });
+        }
+
+        return Promise.resolve({
+            ...returnObj,
+            valid: true
+        });
+    };
 }
 
 export const accessCodeRoutes = new AccessCodesController().setDb(getDBContext()).setMailer(new Mailer()).routes();
